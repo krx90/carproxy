e60e09b4657bf45eab7921a71bdd89a8c85c5bef
krx90 <renke07@outlook.com>
2025-02-17 13:57:14 +0100

    three icons added as buttons

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,10 +1,16 @@
 import './MenuBar.css';
 import image from '../assets/sound.png';
+import image1 from '../assets/chat.png';
+import image2 from '../assets/settings.png';
 
 function MenuBar() {
     return(
-        <div className="menu-bar">
-            <div className = "button-circle"><img src={image} alt="live-button" className="live-button"/></div>
+        <div className='menu-bar'>
+            <div className='menu-icons'>
+                <button className='chat-icon-button'><img src={image1} alt="chat-icon" className="chat-icon"/></button>
+                <button className='live-button-circle'><img src={image} alt="live-button" className="live-button"/></button>
+                <button className='settings-icon-button'><img src={image2} alt="settings-icon" className='settings-icon'/></button>
+            </div>
         </div>
     )
     
