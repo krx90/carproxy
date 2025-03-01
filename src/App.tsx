b66e797e7b1bb402b6d378550d5e29669dc341e3
krx90 <renke07@outlook.com>
2025-03-01 18:49:28 +0100

    added Error Message 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Home from './pages/Home';
 import Chats from './pages/Chats';
 import Settings from './pages/Settings'
+import NoPage from './pages/NoPage';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -9,7 +10,11 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          
+          <Route index element={<Home />} />
+          <Route path='/home' element={<Home />}/>
+          <Route path='/settings' element={<Settings />}/>
+          <Route path='/chats' element={<Chats />}/>
+          <Route path='*' element={<NoPage />}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/NoPage.tsx
@@ -0,0 +1,7 @@
+export default function NoPage() {
+    return(
+        <div>
+            <h1>Error 404: Sorry, Page Not Found</h1>
+        </div>
+    )
+}
