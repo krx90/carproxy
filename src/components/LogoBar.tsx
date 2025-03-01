be2d8d4b5daa1dbdfacb0a89b98bc9c1cd98f82a
krx90 <renke07@outlook.com>
2025-03-01 18:26:16 +0100

    logo bar set lower due to dynamic island and notch on iphone

--- src/components/LogoBar.tsx
+++ src/components/LogoBar.tsx
@@ -4,9 +4,11 @@ import image1 from '../assets/only icon logo.png';
 
 function LogoBar() {
     return (
-        <div className='header-logo'>
-            <img src={image1} alt="logo-icon" className='logo-icon'/>
-            <img src={image} alt="logo-text" className='logo-text'/>
+        <div className='header'>
+            <div className='header-logo'>
+                <img src={image1} alt="logo-icon" className='logo-icon'/>
+                <img src={image} alt="logo-text" className='logo-text'/>
+            </div>
         </div>
     )
 }
