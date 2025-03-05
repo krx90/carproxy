d60a30c46d247703ddbf601b5de31ddef0942ac3
krx90 <renke07@outlook.com>
2025-03-05 13:08:16 +0100

    state management of isonline completed

--- src/components/GoOnline.tsx
+++ src/components/GoOnline.tsx
@@ -1,40 +1,49 @@
 import "./GoOnline.css";
+import React, { useState } from "react";
 import image from "../assets/wifi.png";
 import image1 from "../assets/no-wifi.png";
 
-let IsOnline = false;
-let ConnectedUsers = ["User1", "User2", "User3", "User4"];
+
 
 function GoOnline() {
+  const [IsOnline, setIsOnline] = useState(false);
+  let ConnectedUsers = ["User1", "User2", "User3", "User4"];
+  const ToggleIsOnline = () => {
+    setIsOnline((prevStatus) => !prevStatus);
+  };
+
   if (IsOnline) {
     return (
       <div className="page">
-        <p>Connect to speak with others...</p>
-        <button className="button">
-          <img src={image} alt="button-icon" className="button-icon" />
-          Connect
-        </button>
-      </div>
-      
-    );
-  } else {
-    return (
-      <div className="page">
         <div className="connected-users">
-          {ConnectedUsers.length === 0 ? <p className="list-group">No People Found Nearby</p> : null}
+          {ConnectedUsers.length === 0 ? (
+            <p className="list-group">No People Found Nearby</p>
+          ) : null}
           <ul className="list-group">
             {ConnectedUsers.map((item) => (
               <li key={item}> {item}</li>
             ))}
           </ul>
         </div>
-        <button className="button">
-            <img src={image1} alt="button-icon" className="button-icon" />
-            Disconnect
-          </button>
+        <button className="button" onClick={ToggleIsOnline}>
+          <img src={image1} alt="button-icon" className="button-icon" />
+          Disconnect
+        </button>
       </div>
-    )
+    );
+  } else {
+    return (
+      <div className="page">
+        <p>Connect to speak with others...</p>
+        <button className="button" onClick={ToggleIsOnline}>
+          <img src={image} alt="button-icon" className="button-icon" />
+          Connect
+        </button>
+      </div>
+    );
   }
 }
+
+
 
 export default GoOnline;
